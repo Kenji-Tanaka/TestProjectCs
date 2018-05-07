d3818af73a8313d0a13cf2cdff5ff15ab76ba21c
Kenji Tanaka <Kenji-Tanaka@web.de>
2018-05-07 19:08:56 +0200

    Einen zweiten Versuch wagen

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 dotnet restore --no-cache --force --verbosity normal
 dotnet build --no-restore --no-incremental --verbosity normal --configuration Release
-dotnet test --no-restore --no-build --verbosity normal --configuration Release --test-adapter-path:. --logger:nunit;LogFilePath=..\output\tests\results.xml PrimeService.Tests
+dotnet test --no-restore --no-build --verbosity normal --configuration Release --logger:nunit;LogFilePath=..\output\tests\results.xml PrimeService.Tests
 dotnet pack --no-restore --no-build --verbosity normal --configuration Release --output ..\output\packages\
